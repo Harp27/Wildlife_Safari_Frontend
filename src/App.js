2ba71699aea72582421d13a03e35a325c974ceb3
Harpreet Singh <harpreet95hs@gmail.com>
2023-09-29 16:48:57 -0400

    search bar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
+  // Define a function to handle search
+  const handleSearch = (searchTerm) => {
+    // Implement your filtering logic here and update the UI accordingly.
+    // You can pass the search term to the child routes/components if needed.
+    console.log(`Search term: ${searchTerm}`);
+  };
+
   return (
     <div className="App">
+      <SearchBar className="SearchBar" onSearch={handleSearch} />
       <Outlet />
     </div>
   );
---
+++ src/components/SearchBar.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const SearchBar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearch = () => {
+    onSearch(searchTerm);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search by Endangered Status"
+        value={searchTerm}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+export default SearchBar;
